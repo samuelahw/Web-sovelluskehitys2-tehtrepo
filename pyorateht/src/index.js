3f3a2ca2913225ae83d5631617df50528be26f85
samuelahw <samuel.ahjoniemi@gmail.com>
2021-02-03 13:28:53 +0200

    tehtävä 3 Rest tehty

--- pyorateht/src/index.js
+++ pyorateht/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pyoraparkit from './Pyoraparkit';
+import Kartta from './Kartta';
 
 
 ReactDOM.render(
-  <Pyoraparkit />
+  <div id="mapid">
+   <Kartta/>
+  </div>
+  
   , document.getElementById('root'));
